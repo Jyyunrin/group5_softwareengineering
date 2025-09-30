32c8d8bf73f536be01df811fcdb08d126feb8a06
Inez Yoon <inez3cs@gmail.com>
2025-09-29 18:03:17 -0700

    Set up tailwind css

---
+++ frontend/src/landing.tsx
@@ -0,0 +1,16 @@
+
+import './landing.css'
+
+function Landing() {
+
+
+  return (
+    <>
+      <p className="read-the-docs">
+       main page
+      </p>
+    </>
+  )
+}
+
+export default Landing
