b009d9d33f709c61b45829c5f26fc83cd9e3c884
Inez Yoon <inez3cs@gmail.com>
2025-10-10 19:58:37 -0700

    Add essential files for new users and quiz

---
+++ frontend/src/newuser/quickguide.tsx
@@ -0,0 +1,5 @@
+/**
+ * This page iintroduces how to use the app.
+ * When a new user finishes set up, this page is automaticall pulled.
+ * Users can pull this page anytime. (Click quick guide underneath Hello, Username in landing page)
+ */
